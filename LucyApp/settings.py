3af838daaca23be34f59fe7520fd5ae3ff19d60d
lucasger78 <lucasger78@hotmail.com>
2025-07-16 16:53:45 -0300

    App terminada repo de produccion y despliegue

--- LucyApp/settings.py
+++ LucyApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-@%q7p6x@o00xb&k0-9&m41pfe%t$%#0bau1gix5f6og8=$$^ye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
